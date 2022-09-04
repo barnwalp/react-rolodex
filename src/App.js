1808aaca2d82cee88757d8301a87b210e2269bbc
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-09-04 12:33:41 +0530

    feat/used setState function for other clickhandler

--- src/App.js
+++ src/App.js
@@ -5,48 +5,40 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			toggle: "Yes",
+			name: 'Taanya',
+			job: 'developer',
+			likeReact: "Yes",
 		};
 	}
 	
 	// Here only arrow function work, else it will throw error "can not read property of undefined" as An arrow function does not have its own this; the this value of the enclosing execution context is used. 
-	handleClick = () => {
-		this.setState({toggle: "No"});
+	changeJob = () => {
+		this.setState({job: "Fashionista"});
 	}
-
-	// handleClick() {
-	// 	this.setState((prevData) => {
-	// 		return({
-	// 			...prevData,
-	// 			name: 'sobhita',
-	// 		});
-	// 	});
-	// }
+	
+	// same thing can be implemented by passing a callback function to setState with old state as it's parameter
+	toggleFlag = () => {
+		this.setState(prevData => {
+			return({
+				...prevData,
+				likeReact: "No",
+			});
+		});
+	}
 
 	render() {
 		return (
 			<>
 				<h2>React App</h2>
-				<p>My name is {this.state.toggle} and my job is {this.state.toggle}</p>
-				<button onClick={() => {this.setState({toggle: "No"})}}>Change Name</button>
-				<button onClick={this.handleClick}>Change Name</button>
-				{/*}
-				<button onClick={() => {
-					this.setState(
-						(prevData) => {
-							return{
-								...prevData,
-								name: 'sobhita'
-							}
-						}
-					)
-				}}>
-					Change Name
-				</button>
-				{*/}
+				<p>My name is {this.state.name}</p>
+				<p>my job is {this.state.job}</p>
+				<p>I like React {this.state.likeReact}</p>
+				<button onClick={() => {this.setState({name: "Shobhita"})}}>Change Name</button>
+				<button onClick={this.changeJob}>Change Job</button>
+				<button onClick={this.toggleFlag}>React</button>
 			</>
 		);
-	}
+	};
 }
 
 export default App;
