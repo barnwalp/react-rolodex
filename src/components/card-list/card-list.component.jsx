d61bdc0aab50cfb59769c6355d309d400b92b03f
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-09-07 07:55:33 +0530

    feat/added css file for cards

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import './card-list.styles.css';
+import './card.styles.css';
 
 
 export default class CardList extends React.Component {
 	render() {
-		console.log('render');
 		return (
-			<>
+			<div className="card-list">
 				{this.props.monsters.map((monster) => {
+					// destructuring is done to optimize code
+					const { name, email, id } = monster;
 					return (
-						<div key={monster.id}>
-							<h2>{monster.name}</h2>
+						<div 
+							key={id}
+							className="card-container"	
+						>
+							<img 
+								src={`https://robohash.org/${id}set=set2&size=180x180`} 
+								alt={name} 
+							/>
+							<h2>{name}</h2>
+							<p>{email}</p>
 						</div>
 					)
 				})}
-			</>
+			</div>
 		);
 	};
 };
